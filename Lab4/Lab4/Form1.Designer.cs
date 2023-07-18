8706f8ccba0dd2e8a1da7df60a7f9bdb8e46a584
wyc-wyll <wycheng@bu.edu>
2023-07-18 01:26:37 -0400

    lab4: board is built

---
+++ Lab4/Lab4/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.HintCheckBox = new System.Windows.Forms.CheckBox();
+            this.ResultTextLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(176, 64);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearButton.TabIndex = 0;
+            this.ClearButton.Text = "Clear";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // HintCheckBox
+            // 
+            this.HintCheckBox.AutoSize = true;
+            this.HintCheckBox.Location = new System.Drawing.Point(91, 66);
+            this.HintCheckBox.Name = "HintCheckBox";
+            this.HintCheckBox.Size = new System.Drawing.Size(52, 20);
+            this.HintCheckBox.TabIndex = 1;
+            this.HintCheckBox.Text = "Hint";
+            this.HintCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ResultTextLabel
+            // 
+            this.ResultTextLabel.AutoSize = true;
+            this.ResultTextLabel.Location = new System.Drawing.Point(314, 67);
+            this.ResultTextLabel.Name = "ResultTextLabel";
+            this.ResultTextLabel.Size = new System.Drawing.Size(203, 16);
+            this.ResultTextLabel.TabIndex = 2;
+            this.ResultTextLabel.Text = "You have 0 queens on the board.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultTextLabel);
+            this.Controls.Add(this.HintCheckBox);
+            this.Controls.Add(this.ClearButton);
+            this.Name = "Form1";
+            this.Text = "Eight Queens by Wai Yuen Cheng";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintBoard);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.CheckBox HintCheckBox;
+        private System.Windows.Forms.Label ResultTextLabel;
+    }
+}
+
---
+++ Lab4/Lab4/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab4
+{
+    public partial class Form1 : Form
+    {
+
+        // false => white, true => black
+        private Boolean[,] boardBooleanMatrix = {
+            { false, true, false, true, false, true, false, true },
+            { true, false, true, false, true, false, true, false },
+            { false, true, false, true, false, true, false, true },
+            { true, false, true, false, true, false, true, false },
+            { false, true, false, true, false, true, false, true },
+            { true, false, true, false, true, false, true, false },
+            { false, true, false, true, false, true, false, true },
+            { true, false, true, false, true, false, true, false },
+        };
+
+        private Boolean[,] cellOccupiedMatrix = {
+            { false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false },
+            { false, false, false, false, false, false, false, false }
+        };
+
+        private int boardStartPositionX = 100;
+        private int boardStartPositionY = 100;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void PaintBoard(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = e.Graphics;
+            Pen blackPen = new Pen(Color.FromKnownColor(KnownColor.Black), 2);
+            Brush whiteBrush = new SolidBrush(Color.White);
+            Brush blackBrush = new SolidBrush(Color.Black);
+            Brush redBrush = new SolidBrush(Color.Red);
+
+            Rectangle currentRectangle;
+
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+
+                    currentRectangle = new Rectangle(boardStartPositionX + (i * 50), boardStartPositionY + (j * 50), 50, 50);
+                    g.DrawRectangle(blackPen, currentRectangle);
+
+                    if (boardBooleanMatrix[i, j])
+                    {
+                        g.FillRectangle(blackBrush, currentRectangle);
+                    }
+                    else
+                    {
+                        g.FillRectangle(whiteBrush, currentRectangle);
+                    }
+                }
+            }
+        }
+    }
+}
